0b1b306fd070ffbfa01bcfec3d3a929b6bc520b3
Michal Sorát <michalsorat@gmail.com>
2022-06-14 02:14:28 +0200

    small changes

--- resources/views/views/aboutProject.blade.php
+++ resources/views/views/aboutProject.blade.php
@@ -4,11 +4,7 @@
     <div class="about-project-bnr">
 
     </div>
-{{--    <h1 class="text-center font-weight-bold mt-3">O PROJEKTE</h1>--}}
-{{--    <div class="container mt-5">--}}
-{{--            <h5 class="font-weight-bold">Webová aplikácia Oči na ceste vzniká v spolupráci so Správou a údržbou ciest Trnavského samosprávneho kraja (SÚC TTSK) s cieľom zjednodušiť, zrýchliť a skvalitniť celkové fungovanie verejnej správy. Jej podstata spočíva v zapojení verejnosti do procesu získavania údajov (crowdsourcing) \cite{ref_2} o stave cestnej infraštruktúry a problémoch--}}
-{{--                na nej.</h5>--}}
-{{--    </div>--}}
+
     <div class="about-section container">
         <h1 class="text-center font-weight-bold">O PROJEKTE</h1>
 
@@ -17,7 +13,7 @@
             samosprávneho kraja.</p>
 
         <h2 class="text-center font-weight-bold mt-5 mb-4">Na vývoji tohto projektu sa podielali:</h2>
-        <div class="d-md-flex justify-content-around d-inline-block">
+        <div class="d-md-flex justify-content-around d-inline-block mb-3">
             <div class="card mb-md-0 mb-3" style="width: 18rem">
                 <img class="card-img-top" src="img/developer.jpg" alt="Card image cap">
                 <div class="card-body px-1 py-3">
@@ -27,12 +23,32 @@
                 </div>
             </div>
 
-            <div class="card" style="width: 18rem">
+            <div class="card mb-md-0 mb-3" style="width: 18rem">
                 <img class="card-img-top" src="img/TK.jpg" alt="Card image cap">
                 <div class="card-body px-1 py-3">
                     <h6 class="card-title mb-1 font-weight-bold">Ing. Tomáš Kováčik, PhD</h6>
                     <p class="title font-italic">Supervízor</p>
                     <p class="card-text">Vedúci bakalárskeho projektu Oči&nbsp;na&nbsp;ceste, koordinátor a kreatívny člen tímu.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-md-flex justify-content-around d-inline-block mb-3">
+            <div class="card mb-md-0 mb-3" style="width: 18rem">
+                <img class="card-img-top" src="img/user.png" alt="Card image cap">
+                <div class="card-body px-1 py-3">
+                    <h6 class="card-title mb-1 font-weight-bold">Bc. Denis Piovarči</h6>
+                    <p class="title font-italic">Vývojár</p>
+                    <p class="card-text">Študent na FIIT STU v Bratislave. Vývojár pôvodnej verzie bakalárskeho projektu Oči&nbsp;na&nbsp;ceste v rokoch 2019/2020.</p>
+                </div>
+            </div>
+
+            <div class="card mb-md-0 mb-3" style="width: 18rem">
+                <img class="card-img-top" src="img/user.png" alt="Card image cap">
+                <div class="card-body px-1 py-3">
+                    <h6 class="card-title mb-1 font-weight-bold">Bc. Šimon Drienik</h6>
+                    <p class="title font-italic">Vývojár</p>
+                    <p class="card-text">Študent FIIT STU v Bratislave. Vývojár mobilnej aplikácie Oči&nbsp;na&nbsp;ceste v rámci bakalárskeho projektu v rokoch 2020/2021.</p>
                 </div>
             </div>
         </div>
--- resources/views/views/mobileAppInfo.blade.php
+++ resources/views/views/mobileAppInfo.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>app</h1>
+    <div class="about-project-bnr">
+
+    </div>
+
+    <div class="about-section container">
+        <h1 class="text-center font-weight-bold mb-5">ANDROID APLIKÁCIA</h1>
+
+        <div class="d-md-flex justify-content-around d-inline-block mb-3">
+            <img src="img/developer.jpg" alt="Card image cap">
+
+            <div class="card mb-md-0 mb-3" style="width: 18rem">
+                <img class="card-img-top" src="img/TK.jpg" alt="Card image cap">
+                <div class="card-body px-1 py-3">
+                    <h6 class="card-title mb-1 font-weight-bold">Ing. Tomáš Kováčik, PhD</h6>
+                    <p class="title font-italic">Supervízor</p>
+                    <p class="card-text">Vedúci bakalárskeho projektu Oči&nbsp;na&nbsp;ceste, koordinátor a kreatívny člen tímu.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 @endsection
